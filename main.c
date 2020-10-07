10357cd1ad5edc58c08d488a5e036873bbe0c3fc
NazarBabiak <nazar.babiak.lv@gmail.com>
2020-10-07 22:47:52 +0300

    The following changes have been made:     - Update .gitignore file     - Add .lib files for download required libraries     - Add MakeFile     - Add main.c (Blinking Led)

---
+++ main.c
@@ -0,0 +1,78 @@
+/******************************************************************************
+* (c) 2019-2020, Cypress Semiconductor Corporation. All rights reserved.
+*******************************************************************************
+* This software, including source code, documentation and related materials
+* ("Software"), is owned by Cypress Semiconductor Corporation or one of its
+* subsidiaries ("Cypress") and is protected by and subject to worldwide patent
+* protection (United States and foreign), United States copyright laws and
+* international treaty provisions. Therefore, you may use this Software only
+* as provided in the license agreement accompanying the software package from
+* which you obtained this Software ("EULA").
+*
+* If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
+* non-transferable license to copy, modify, and compile the Software source
+* code solely for use in connection with Cypress's integrated circuit products.
+* Any reproduction, modification, translation, compilation, or representation
+* of this Software except as specified above is prohibited without the express
+* written permission of Cypress.
+*
+* Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
+* reserves the right to make changes to the Software without notice. Cypress
+* does not assume any liability arising out of the application or use of the
+* Software or any product or circuit described in the Software. Cypress does
+* not authorize its products for use in any products where a malfunction or
+* failure of the Cypress product may reasonably be expected to result in
+* significant property damage, injury or death ("High Risk Product"). By
+* including Cypress's product in a High Risk Product, the manufacturer of such
+* system or application assumes all risk of such use and in doing so agrees to
+* indemnify Cypress against all liability.
+*******************************************************************************/
+
+#include "cy_pdl.h"
+
+
+/* Pointer to GPIO Block */
+#define USER_LED_PORT                           (GPIO_PRT13)
+/* Pin number */
+#define USER_LED_PIN                            (7U)
+#define DELAY                                   (500U)
+
+
+/* Config structure for user led pin */
+static cy_stc_gpio_pin_config_t user_led =
+{
+    .outVal = 1,
+	.driveMode = CY_GPIO_DM_STRONG_IN_OFF,
+	.hsiom = P13_4_GPIO,
+	.intEdge = CY_GPIO_INTR_DISABLE,
+	.intMask = 0UL,
+	.vtrip = CY_GPIO_VTRIP_CMOS,
+	.slewRate = CY_GPIO_SLEW_FAST,
+	.driveSel = CY_GPIO_DRIVE_1_2,
+	.vregEn = 0UL,
+	.ibufMode = 0UL,
+	.vtripSel = 0UL,
+	.vrefSel = 0UL,
+	.vohSel = 0UL,
+};
+
+
+int main(void)
+{
+
+    /* Init user led pin */
+    Cy_GPIO_Pin_Init(USER_LED_PORT, USER_LED_PIN, &user_led);
+
+
+    for (;;)
+    {
+        Cy_SysLib_Delay(DELAY);
+
+		/* Invert pin */
+        Cy_GPIO_Inv(USER_LED_PORT, USER_LED_PIN);
+    }
+}
+
+/* [] END OF FILE */
