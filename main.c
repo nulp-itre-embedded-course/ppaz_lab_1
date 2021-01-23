da0184f07261d4e92f55bf2d3f0a414d1b479bbc
AndruxaKushyk <andriy35y@gmail.com>
2021-01-23 15:57:04 +0200

    lab1

--- main.c
+++ main.c
@@ -1,27 +1,18 @@
-/* ========================================
- *
- * Copyright YOUR COMPANY, THE YEAR
- * All Rights Reserved
- * UNPUBLISHED, LICENSED SOFTWARE.
- *
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
- *
- * ========================================
-*/
 #include "project.h"
+
+#define HIGH 1
+#define LOW  0
 
 int main(void)
 {
-    CyGlobalIntEnable; /* Enable global interrupts. */
 
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    CyGlobalIntEnable; /* Enable global interrupts. */
 
     for(;;)
     {
-        Diod_Write(!Diod_Read());
-        CyDelay(5000);
+        LED_Write (HIGH); // Встановлення високого потенціалу на пін LED
+        CyDelay(1000);// функцiя затримки
+        LED_Write (LOW); // Встановлення низького потенціалу на пін LED
+        CyDelay(1000);  
     }
 }
-
-/* [] END OF FILE */
